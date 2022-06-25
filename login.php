c2e5848b40920f0f97e2051380395f9352d04fcd
Aliflkpd <andialifnurman@gmail.com>
2022-06-25 21:21:34 +0800

    p

--- login.php
+++ login.php
@@ -4,13 +4,30 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+</head>
     <tittle>Login</tittle>
 </head>
 <body>
 <div class="main">
     <div class="div-content">
-        <h1>Register</h1>
+        <h1>Login</h1>
         <div class="div-box">
+        <form action="" method="post">
+                    <div>
+                        <label for="email">Email</label>
+                        <input type="email" name="email" id="email">
+                    </div>
+                   
+                    <div>
+                        <label for="password">Password</label>
+                        <input type="password" name="password" id="password">
+                    </div>
+                    
+                    <div>
+                        <input type="submit" name="submit" value="Register">
+                    </div>
+                </form>
         </div>
     </div>
 </div>
