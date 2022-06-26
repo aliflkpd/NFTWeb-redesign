235a68a365a76af5d7c051bc7fc6ec82e1e3f272
Aliflkpd <andialifnurman@gmail.com>
2022-06-26 17:12:27 +0800

    WalletID added into database

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require "session.php";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <tittle>Home</tittle>
+    <tittle></tittle>
 </head>
 
 <Style>
@@ -20,6 +20,6 @@ require "session.php";
     }
 </Style>
 <body>
-    <h1>Welcome, <?php echo $_SESSION['email']; ?></h1>
+    <h1>Welcome, <?php echo $_SESSION['WalletID']; ?></h1>
 </body>
 </html>
--- register.php
+++ register.php
@@ -21,6 +21,11 @@
                     </div>
                    
                     <div>
+                        <label for="WalletID">WalletID</label>
+                        <input type="WalletID" name="WalletID" id="WalletID">
+                    </div>
+
+                    <div>
                         <label for="password">Password</label>
                         <input type="password" name="password" id="password">
                     </div>
@@ -33,24 +38,27 @@
                 <?php 
                     if(isset($_POST['submit'])){
                         $email = htmlspecialchars($_POST['email']);
+                        $WalletID = htmlspecialchars($_POST['WalletID']);
                         $password = htmlspecialchars($_POST['password']);
                         $encryptedPassword = password_hash($password, PASSWORD_DEFAULT);
                     
                         $query = mysqli_query($con, "SELECT email FROM users WHERE email='$email'");
+                        $query = mysqli_query($con, "SELECT WalletID FROM users WHERE WalletID='$WalletID'");
                         $count = mysqli_num_rows($query);
+                        
 
                         if($count > 0){
-                            echo "Email already registered!";
+                            echo "Email or WalletID already registered!";
                         }
                         else {
-                            $queryInsert = mysqli_query($con, "INSERT INTO users (email,password) VALUES 
-                            ('$email','$encryptedPassword')");
+                            $queryInsert = mysqli_query($con, "INSERT INTO users (email,password,WalletID) VALUES 
+                            ('$email','$encryptedPassword','$WalletID')");
                         
-                        }
                         IF($queryInsert) {
                             echo "Register success!";
                             echo '<a href="login.php"><br>Click here to login</a>';
                         }
+                    }
                     }
                 ?>
             </div>
