d6f9d3cdf5455b67c5cdba6552d2f080c5dff6a5
Aliflkpd <andialifnurman@gmail.com>
2022-06-27 22:01:43 +0800

    p

--- about.php
+++ about.php
@@ -1,14 +1,35 @@
 <!doctype html>
 <html lang="en">
  <head>
+ <link rel="stylesheet" href="style.css">
  <?php
     include "header.php";
-    ?>
+ ?>
+
 
   </head> 
 
-  <body>
-  <h1><img src="img/about2.png" width=100%></h1>
+  <body style="background-color: #ff6f00">
+  
+  <div class="navigation">
+  <a class="button" href="logout.php"><div class="logout">LOGOUT</div> </a>
+  </div>
+</h2>
+<h3><br/><br/><br/><br/>
+
+    <div class="navigation2">
+        <a class="button" href="index.php"><div class="Our NFTs">Our NFTs</div> </a>
+</h3>
+<br/><br/><br/><br/><br/><br/>
+
+  <h1><CENTER>ABOUT US</CENTER><hr>
+  <div class="containerfoto" style="width: 100%; 
+    height: 300px;
+    background: none";><img src="img/about2.png" style="width:100%; border: 2px solid cyan;
+  border-radius: 8px;"></h1>
+   
+    <br/><br/><BR/><br/><br/><br/><BR/><br/><br/><br/>
+    <h3></h3><center>DISUSUN OLEH : <br/> KELOMPOK TIDAK KENAL PULANG <BR/> Andi Alif Lakipadada (42030077) <BR/> Risky Putra Prayoga (42030051) <br/> Ketut Yudiantara (42030057) <br/> Rayhan Mahaputra (42030049) <br/> Krisna Danendra (42030059)</center></h3>
   </body>
  <?php
     include "footer.php";
