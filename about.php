6a00af71440f1785fa7f520f8075a81f388d3493
imaderiskyputraprayoga@gmail.com <imaderiskyputraprayoga@gmail.com>
2022-06-27 22:30:26 +0800

    gg gemink

--- about.php
+++ about.php
@@ -2,6 +2,9 @@
 <html lang="en">
  <head>
  <link rel="stylesheet" href="style.css">
+ <link rel="preconnect" href="https://fonts.googleapis.com">
+ <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+ <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&family=Roboto:ital,wght@0,100;0,300;1,100&family=Trispace:wght@500;600;800&family=VT323&display=swap" rel="stylesheet">
  <?php
     include "header.php";
  ?>
@@ -21,15 +24,12 @@
         <a class="button" href="index.php"><div class="Our NFTs">Our NFTs</div> </a>
 </h3>
 <br/><br/><br/><br/><br/><br/>
-
+  <div class="containerfoto">
   <h1><CENTER>ABOUT US</CENTER><hr>
-  <div class="containerfoto" style="width: 100%; 
-    height: 300px;
-    background: none";><img src="img/about2.png" style="width:100%; border: 2px solid cyan;
-  border-radius: 8px;"></h1>
-   
-    <br/><br/><BR/><br/><br/><br/><BR/><br/><br/><br/>
-    <h3></h3><center>DISUSUN OLEH : <br/> KELOMPOK TIDAK KENAL PULANG <BR/> Andi Alif Lakipadada (42030077) <BR/> Risky Putra Prayoga (42030051) <br/> Ketut Yudiantara (42030057) <br/> Rayhan Mahaputra (42030049) <br/> Krisna Danendra (42030059)</center></h3>
+  <h1><img src="img/about2.png" style="width:100%; border: 2px solid cyan;
+  border-radius: 8px;"></div></h1>
+   <div class="namakelompok">
+    <h3></h3><center>DISUSUN OLEH : <br/> KELOMPOK TIDAK KENAL PULANG <BR/> Andi Alif Lakipadada (42030077) <BR/> Risky Putra Prayoga (42030051) <br/> Ketut Yudiantara (42030057) <br/> Rayhan Mahaputra (42030049) <br/> Krisna Danendra (42030059)</center></div></h3>
   </body>
  <?php
     include "footer.php";
