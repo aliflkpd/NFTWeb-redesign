2d7d9b7435d8025c6c8d7a0a2982f3a05028686b
Aliflkpd <andialifnurman@gmail.com>
2022-06-28 12:22:54 +0800

    DONE

--- buy.php
+++ buy.php
@@ -3,17 +3,28 @@
  <head>
  <link rel="stylesheet" href="style.css">
  <link rel="preconnect" href="https://fonts.googleapis.com">
- <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
- <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&family=Roboto:ital,wght@0,100;0,300;1,100&family=Trispace:wght@500;600;800&family=VT323&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&family=Roboto:ital,wght@0,100;0,300;1,100&family=Trispace:wght@500;600;800&family=VT323&display=swap" rel="stylesheet">
  <?php
     include "header.php";
  ?>
- 
+ <style>
+   .purchasingbox {
+      font-size: 25px;
+  font-family: 'Indie Flower', cursive;
+    font-family: 'Roboto', sans-serif;
+    font-family: 'Trispace', sans-serif;
+    font-family: 'VT323', monospace;
+    text-shadow: 2px 2px 4px #8a8a8a;
+   }
+
+ </style>
  <div class="center">
         <h1><img src="text.jpg" width=100% height=100%></h1></div>
  </head> 
     
  <body style="background-color: #ff6f00">
+ 
     <div class="purchasingbox"><div class="navigation2">
         <a class="button" href="index.php"><div class="Our NFTs">Back</div> </a></div>
 </h3>
@@ -21,6 +32,7 @@
   <h1><div class="textborder"> <CENTER>Thanks for purchasing! <br/>Your transaction is on progress,<br/> please wait until we send to your wallet.</CENTER></div></h1>
 
     </div>
+
   </body>
  <?php
     include "footer.php";
